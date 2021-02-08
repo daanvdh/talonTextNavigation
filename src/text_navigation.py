d4ea90727f5005b29d99b8d78993dd544929b9cc
Daan van den Heuvel <d.vandenheuvel@eyefreight.com>
2021-02-08 19:49:02 +0100

    Single python entry point, implemented extra options

--- src/text_navigation.py
+++ src/text_navigation.py
@@ -4,44 +4,71 @@ from talon import ctrl, ui, Module, Context, actions, clip
 import itertools
 from _ast import Try
 
+ctx = Context()
 mod = Module()
+
+mod.list("cursor_location", desc="words to start dictating prose, and the formatter they apply")
+mod.list("direction", desc="words to start dictating prose, and the formatter they apply")
+mod.list("navigation_option", desc="words to start dictating prose, and the formatter they apply")
+ctx.lists["self.cursor_location"] = {"before", "after"}
+ctx.lists["self.direction"] = {"left", "right"}
+ctx.lists["self.navigation_option"] = {"go", "extend", "select", "delete", "cut"}
+
+@mod.capture(rule="{self.cursor_location}")
+def cursor_location(m) -> str:
+    "One directional arrow key"
+    return m.cursor_location
+@mod.capture(rule="{self.direction}")
+def direction(m) -> str:
+    "One directional arrow key"
+    return m.direction
+@mod.capture(rule="{self.navigation_option}")
+def navigation_option(m) -> str:
+    "One directional arrow key"
+    return m.navigation_option
+
 @mod.action_class
 class Actions:
+    def navigation(navigation_option: str, direction: str, cursor_location: str, text: str, occurrence_number: int):
+        """go right until you find the given symbol for the occurrence_number-th time and put the cursor before it"""
+        option = navigation_option+" "+cursor_location if navigation_option=="go" else navigation_option
+        navigation(option, direction, re.escape(text), int(occurrence_number))
+
     def move_right_before(symbol: str, occurrence_number: int):
         """go right until you find the given symbol for the occurrence_number-th time and put the cursor before it"""
-        before(re.escape(symbol), int(occurrence_number))
+#        before(re.escape(symbol), int(occurrence_number))
         
     def move_right_after(symbol: str, occurrence_number: int):
         """go right until you find the given symbol for the occurrence_number-th time and put the cursor after it"""
-        after(ascii(symbol), int(occurrence_number))
+#        after(ascii(symbol), int(occurrence_number))
 
     def move_left_before(symbol: str, occurrence_number: int):
         """go left until you find the given symbol for the occurrence_number-th time and put the cursor before it"""
-        backwards_before(re.escape(symbol), occurrence_number)
+#        backwards_before(re.escape(symbol), occurrence_number)
         
     def move_left_after(symbol: str, occurrence_number: int):
         """go left until you find the given symbol for the occurrence_number-th time and put the cursor after it"""
-        backwards_after(ascii(symbol), int(occurrence_number))        
+#        backwards_after(ascii(symbol), int(occurrence_number))        
                         
     def extend_right_before(symbol: str, occurrence_number: int):
         """go right until you find the given symbol for the occurrence_number-th time and extent the current selection until before it"""
-        extend_right_before(ascii(symbol), int(occurrence_number))
+#        extend_right_before(ascii(symbol), int(occurrence_number))
                         
     def extend_right_after(symbol: str, occurrence_number: int):
         """go right until you find the given symbol for the occurrence_number-th time and extent the current selection until after it"""
-        extend_right_after(ascii(symbol), int(occurrence_number))
+#        extend_right_after(ascii(symbol), int(occurrence_number))
 
     def extend_left_before(symbol: str, occurrence_number: int):
         """go left until you find the given symbol for the occurrence_number-th time and extent the current selection until before it"""
-        extend_left_before(ascii(symbol), int(occurrence_number))
+#        extend_left_before(ascii(symbol), int(occurrence_number))
 
     def extend_left_after(symbol: str, occurrence_number: int):
         """go left until you find the given symbol for the occurrence_number-th time and extent the current selection until after it"""
-        extend_left_after(ascii(symbol), int(occurrence_number))
+#        extend_left_after(ascii(symbol), int(occurrence_number))
 
     def select_right(text: str, occurrence_number: int):
         """go left until you find the given symbol for the occurrence_number-th time and extent the current selection until after it"""
-        select_right(re.escape(text), occurrence_number)
+#        select_right(re.escape(text), occurrence_number)
                                 
 def get_text_left():
     actions.edit.extend_line_start()
@@ -58,33 +85,6 @@ def get_text_right():
 def get_current_selection_size():
     return len(actions.edit.selected_text())
 
-# to do: remove method
-def find_index_right(symbol, occurrence_number, text, start_index):
-    i = start_index
-    occurrence_count = 0
-    while i < len(text) and occurrence_count != occurrence_number:
-        if symbol[1] == text[i]:
-            occurrence_count += 1
-            if occurrence_count != occurrence_number:
-                i += 1
-        else:
-            i += 1
-    return i if occurrence_count == occurrence_number else -1
-
-# to do: remove method
-def find_index_left(symbol, occurrence_number, text, start_index):
-    i = start_index
-    occurrence_count = 0
-    length = len(text) - 1
-    while i < length and occurrence_count != occurrence_number:
-        if symbol[1] == text[length - i]:
-            occurrence_count += 1
-            if occurrence_count != occurrence_number:
-                i += 1
-        else:
-            i += 1
-    return i if occurrence_count == occurrence_number else -1
-
 def go_right(i):
     for j in range(0, i):
         actions.edit.right()
@@ -93,91 +93,83 @@ def go_left(i):
     for j in range(0, i):
         actions.edit.left()
 
+def extend_left(i):
+    for j in range(0, i):
+        actions.edit.extend_left()
 
 def extend_right(i):
     for j in range(0, i):
         actions.edit.extend_right()
 
-def extend_right_or_reselect(current_selection_length, i):
-    if i > 0:
-        extend_right(i)
+def navigation(navigation_option, direction, regex, occurrence_number):
+    if direction == "left":
+        navigate_left(navigation_option, regex, occurrence_number)
     else:
-        for j in range(0, current_selection_length):
-            actions.edit.extend_right()
+        navigate_right(navigation_option, regex, occurrence_number)
 
-def extend_left_or_reselect(current_selection_length, i):
-    if i > 0:
-        for j in range(0, i):
-            actions.edit.extend_left()
-    else:
-        for j in range(0, current_selection_length):
-            actions.edit.extend_left()
-
-def before(regex, occurrence_number):
-    text = get_text_right()
-    try:
-        # 2 do: remove comments
-        # pick the next interrater, Skip n number of occurrences, get an iterator given the Regex
-        match = next(itertools.islice(re.finditer(regex, text), occurrence_number-1, None))
-    except StopIteration:
-        return 
-    go_right(match.start())
-
-def after(symbol, occurrence_number):
-    text = get_text_right()
-    i = find_index_right(symbol, occurrence_number, text, 0)
-    if i > 0: go_right(i+1)
-
-def backwards_after(symbol, occurrence_number):
-    text = get_text_left()
-    i = find_index_left(symbol, occurrence_number, text, 0)
-    if i > 0: go_left(i)
-    
-def backwards_before(regex, occurrence_number):
-    text = get_text_left()
-    try:
-        match = list(re.finditer(regex, text))[-occurrence_number]
-    except IndexError:
-        return 
-    go_left(len(text)- match.end())
-
-def extend_right_before(symbol, occurrence_number):
+def navigate_left(navigation_option, regex, occurrence_number):
     current_selection_length = get_current_selection_size()
     if current_selection_length > 0: 
-        actions.edit.left()        
-    text = get_text_right()
-    i = find_index_right(symbol, occurrence_number, text, current_selection_length)
-    extend_right_or_reselect(current_selection_length, i)
+        actions.edit.right()
+    try:
+        text = get_text_left()
+        subtext = text if current_selection_length <= 0 else text[:-current_selection_length] 
+        match = list(re.finditer(regex, subtext, re.IGNORECASE))[-occurrence_number]
+        start = match.start() 
+        end = match.end()
+        if navigation_option == "go before":
+            go_left(len(text)- start)                
+        elif navigation_option == "go after":
+            go_left(len(text)- end)
+        elif navigation_option == "select":
+            go_left(len(text)- end)                
+            extend_left(end-start)
+        elif navigation_option == "delete":
+            go_left(len(text)- end)                
+            extend_left(end-start)
+            actions.edit.delete()
+        elif navigation_option == "cut":
+            go_left(len(text)- end)                
+            extend_left(end-start)
+            actions.edit.cut()
+        elif navigation_option == "extend":
+            extend_left(len(text) - match.start())
+    except IndexError:
+        extend_left(current_selection_length)
+        return 
+    return
 
-def extend_right_after(symbol, occurrence_number):
-    current_selection_length = get_current_selection_size()
+def navigate_right(navigation_option, regex, occurrence_number):
+    current_selection_length = get_current_selection_size()                
     if current_selection_length > 0: 
         actions.edit.left()
-    text = get_text_right()
-    i = find_index_right(symbol, occurrence_number, text, current_selection_length)
-    extend_right_or_reselect(current_selection_length, i+1)
-
-def extend_left_before(symbol, occurrence_number):
-    current_selection_length = get_current_selection_size()
-    if current_selection_length > 0: 
-        actions.edit.right()
-    text = get_text_left()
-    i = find_index_left(symbol, occurrence_number, text, current_selection_length)
-    extend_left_or_reselect(current_selection_length, i+1)
-
-def extend_left_after(symbol, occurrence_number):
-    current_selection_length = get_current_selection_size()
-    if current_selection_length > 0: 
-        actions.edit.right()
-    text = get_text_left()
-    i = find_index_left(symbol, occurrence_number, text, current_selection_length)    
-    extend_left_or_reselect(current_selection_length, i)
-
-def select_right(regex, occurrence_number):
-    text = get_text_right()
     try:
-        match = next(itertools.islice(re.finditer(regex, text, re.IGNORECASE), occurrence_number-1, None))
+        text = get_text_right()
+        sub_text = text[current_selection_length:]
+        # pick the next interrater, Skip n number of occurrences, get an iterator given the Regex
+        match = next(itertools.islice(re.finditer(regex, sub_text, re.IGNORECASE), occurrence_number-1, None))
+        start = current_selection_length + match.start() 
+        end = current_selection_length + match.end() 
+        if navigation_option == "go before":
+            go_right(start)
+        elif navigation_option == "go after":
+            go_right(end)
+        elif navigation_option == "select":
+            go_right(start)
+            extend_right(end-start)
+        elif navigation_option == "delete":
+            go_right(start)
+            extend_right(end-start)
+            actions.edit.delete()
+        elif navigation_option == "cut":
+            go_right(start)
+            extend_right(end-start)
+            actions.edit.cut()
+        elif navigation_option == "extend":
+            extend_right(end)
     except StopIteration:
-        return 
-    go_right(match.start())
-    extend_right(match.end()-match.start())
+        extend_right(current_selection_length)
+        return
+    return 
+
+    
--- src/text_navigation.talon
+++ src/text_navigation.talon
@@ -1,13 +1,24 @@
 
 #column navigation    
 #Consider renaming before and after to till and past
-[go] [right] before <user.symbol_key> [<number>]: user.move_right_before(symbol_key, number or 1)
-[go] [right] after <user.symbol_key> [<number>]: user.move_right_after(symbol_key, number or 1)
-[go] left [before] <user.symbol_key> [<number>]: user.move_left_before(symbol_key, number or 1)
-[go] left after <user.symbol_key> [<number>]: user.move_left_after(symbol_key, number or 1)
-extend [right] before <user.symbol_key> [<number>]: user.extend_right_before(symbol_key, number or 1)
-extend [right] [after] <user.symbol_key> [<number>]: user.extend_right_after(symbol_key, number or 1)
-extend left [before] <user.symbol_key> [<number>]: user.extend_left_before(symbol_key, number or 1)
-extend left after <user.symbol_key> [<number>]: user.extend_left_after(symbol_key, number or 1)
+#[go] [right] before <user.symbol_key> [<number>]: user.move_right_before(symbol_key, number or 1)
+#[go] [right] after <user.symbol_key> [<number>]: user.move_right_after(symbol_key, number or 1)
+#[go] left [before] <user.symbol_key> [<number>]: user.move_left_before(symbol_key, number or 1)
+#[go] left after <user.symbol_key> [<number>]: user.move_left_after(symbol_key, number or 1)
+#extend [right] before <user.symbol_key> [<number>]: user.extend_right_before(symbol_key, number or 1)
+#extend [right] [after] <user.symbol_key> [<number>]: user.extend_right_after(symbol_key, number or 1)
+#extend left [before] <user.symbol_key> [<number>]: user.extend_left_before(symbol_key, number or 1)
+#extend left after <user.symbol_key> [<number>]: user.extend_left_after(symbol_key, number or 1)
 
-Select <user.text> [<number>]: user.select_right(text, number or 1)
+# text navigation
+[<user.navigation_option>] [<user.direction>] <user.cursor_location> <user.text> [<number>]:
+	user.navigation(navigation_option or "go", direction or "right", cursor_location, text, number or  1)
+<user.navigation_option> [<user.direction>] <user.text> [<number>]:
+	user.navigation(navigation_option, direction or "right", "after", text, number or  1)
+	
+# symbol navigation
+[<user.navigation_option>] [<user.direction>] <user.cursor_location> <user.symbol_key> [<number>]:
+	user.navigation(navigation_option or "go", direction or "right", cursor_location, symbol_key, number or  1)
+<user.navigation_option> [<user.direction>] <user.symbol_key> [<number>]:
+	user.navigation(navigation_option, direction or "right", "after", symbol_key, number or  1)
+	
